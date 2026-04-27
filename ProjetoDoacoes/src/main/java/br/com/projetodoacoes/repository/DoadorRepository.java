6efa6a2fd56f2c68147cead10468bdcab3fc5f61
mat9831 <134665795+mat9831@users.noreply.github.com>
2026-04-27 11:52:43 -0300

    Delete ProjetoDoacoes directory

--- ProjetoDoacoes/src/main/java/br/com/projetodoacoes/repository/DoadorRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.projetodoacoes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.projetodoacoes.model.Doador;
-
-public interface DoadorRepository extends JpaRepository<Doador, Long> {
-}
